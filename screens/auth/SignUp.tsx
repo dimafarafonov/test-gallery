b9b2857d9842a3c32ee0c10f8f2735874c6d3124
DiiF <dimafarafonovzt@gmail.com>
2025-06-22 11:52:32 +0300

    refactoring

---
+++ screens/auth/SignUp.tsx
@@ -0,0 +1,43 @@
+import { useAuth } from "@/providers/auth/hooks/useAuth";
+import { useState, useCallback } from "react";
+import { KeyboardAvoidingView, Platform, StyleSheet } from "react-native";
+import { FormContainer } from "./fractions/FormContent";
+
+const SignUpScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signUp } = useAuth();
+
+  const handleSignUp = useCallback(() => {
+    signUp({ email, password });
+  }, [email, password, signUp]);
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+    >
+      <FormContainer
+        redirectRoute={"/auth/sign-in"}
+        redirectLabel="Sign in"
+        email={email}
+        password={password}
+        setEmail={setEmail}
+        onContinue={handleSignUp}
+        setPassword={setPassword}
+      />
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+  },
+});
+
+export { SignUpScreen };
